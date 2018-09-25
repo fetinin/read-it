51ff505b71bac103ca8df5090ca04bc509b41a08
Denis <denka0087@gmail.com>
2018-09-25 13:37:26 +0300

    Enhance upload book form

--- frontend/src/views/UploadBook.vue
+++ frontend/src/views/UploadBook.vue
@@ -3,34 +3,50 @@
   <loader-modal ref='loader'></loader-modal>
 
     <form class="form-group" @submit.prevent="save" enctype="multipart/form-data">
-        <h2 class="header">Загрузить книгу</h2>
-        <label class="form-label" for="title">Название</label>
-        <input v-model="title" class="form-input" type="text" id="title" placeholder="A Good Man Is Hard to Find and Other Stories" required>
-        <label class="form-label" for="author">Автор</label>
-        <input v-model='author' class="form-input" type="text" id="author" placeholder="Flannery O'Connor" required>
+        <h2 v-if="currentStep === 1" class="header">Выбери книгу</h2>
+        <h2 v-else-if="currentStep === 2" class="header">Добавь название</h2>
 
-        <div class="dropbox">
-          <input type="file" :disabled="isSaving" @change="onBookUpload($event.target.files)"
-            accept="application/pdf,text/*,.epub" class="input-file" id="book" rяequired>
-            <p v-if="isInitial">
-              Перетащите вашу книгу сюда<br> или кликните чтобы открыть обозреватель
+        <ul class="step">
+          <li :class="['step-item', {'active': currentStep === 1}]">
+            <a href="#" class="tooltip" data-tooltip="Выбери книгу">Шаг 1</a>
+          </li>
+          <li :class="['step-item', {'active': currentStep === 2}]">
+            <a href="#" class="tooltip" data-tooltip="Задай название и обложку">Шаг 2</a>
+          </li>
+        </ul>
+
+        <div id="step1" v-if="currentStep === 1">
+          <div class="dropbox">
+            <input type="file" @change="onBookUpload($event.target.files)"
+              accept="application/pdf,.txt,.epub" class="input-file" id="book" rяequired>
+            <p>
+              Перетащите вашу книгу сюда<br> или кликните чтобы открыть обозреватель.<br>
+              Сейчас поддерживаются форматы: pdf, epub, txt.
             </p>
-            <p v-else>
-                {{bookFile.name}}
-            </p>
+          </div>
         </div>
 
-        <div class="dropbox">
-          <input type="file" :disabled="isSaving" @change="onBookCoverUpload($event.target.files)"
-            accept="image/*" class="input-file" id="book-cover">
-            <p v-if="!bookCoverFile.name">
-              Перетащите обложку книги сюда<br> или кликните чтобы открыть обозреватель
-            </p>
-            <p v-else>
-                {{bookCoverFile.name}}
-            </p>
+        <div id="step2"  v-else-if="currentStep === 2">
+          <div class="columns">
+            <div class="column col-8 col-sm-12">
+              <label class="form-label" for="title">Название</label>
+              <input v-model="title" class="form-input" type="text" id="title" placeholder="A Good Man Is Hard to Find and Other Stories" required>
+              <label class="form-label" for="author">Автор</label>
+              <input v-model='author' class="form-input" type="text" id="author" placeholder="Flannery O'Connor" required>
+            </div>
+
+            <div class="column col-4 col-sm-12">
+              <label class="form-label text-center" for="cover">Обложка</label>
+              <div :class="['dropbox-img', 'text-center', {'dropbox-no-image':  !coverImgData}]" :style="{ 'background-image': `url(${coverImgData})` }">
+                <input type="file" @change="onBookCoverUpload($event.target.files)" accept="image/*" class="input-file" id="book-cover">
+                <p v-if="!coverImgData">
+                  Перетащите обложку книги сюда<br> или кликните чтобы открыть обозреватель
+                </p>
+              </div>
+            </div>
+          </div>
+          <button class="btn btn-primary">Готово!</button>
         </div>
-        <button class="btn btn-primary">Загрузить</button>
     </form>
 </div>
 </template>
@@ -40,7 +56,7 @@ import { Component, Vue } from 'vue-property-decorator';
 import { File, Book } from '@/types.ts';
 import LoaderModal from '@/components/LoaderModal.vue';
 
-const STATUS = {
+const FILE_LOAD_STATUS = {
   INITIAL: 0,
   SAVING: 1,
   SUCCESS: 2,
@@ -55,23 +71,13 @@ export default class BookUpload extends Vue {
   public author = '';
   public bookFile: File = { name: '', reader: new FileReader() };
   public bookCoverFile: File = { name: '', reader: new FileReader() };
-  public currentStatus = STATUS.INITIAL;
+  public coverImgData: null | string = null;
 
-  get isInitial() {
-    return this.currentStatus === STATUS.INITIAL;
-  }
-  get isSaving() {
-    return this.bookFile.reader.readyState === this.bookFile.reader.LOADING;
-  }
-  get isSuccess() {
-    return this.bookFile.reader.readyState === this.bookFile.reader.DONE;
-  }
-  get isFailed() {
-    return this.currentStatus === STATUS.FAILED;
-  }
+  private currentStep = 1;
 
   private save(formData: any) {
-    (this.$refs.loader as LoaderModal).show();
+    const loader = this.$refs.loader as LoaderModal;
+    loader.show();
     this.$http
       .post(
         '/books',
@@ -80,7 +86,7 @@ export default class BookUpload extends Vue {
           author: this.author,
           file: (this.bookFile.reader.result as string).split('base64,').pop(),
           format: this.bookFile.name.split('.').pop(),
-          cover: this.bookCoverFile.reader.result,
+          cover: this.coverImgData,
         },
         { timeout: 120000 }, // 2 minutes timeout
       )
@@ -91,7 +97,7 @@ export default class BookUpload extends Vue {
       .catch((err) => {
         console.log(err.response);
       })
-      .finally(() => (this.$refs.loader as LoaderModal).hide());
+      .finally(() => loader.hide());
   }
 
   private onBookUpload(files: FileList) {
@@ -101,7 +107,7 @@ export default class BookUpload extends Vue {
     const file = files[0];
     this.bookFile.name = file.name;
     this.bookFile.reader.readAsDataURL(file);
-    this.currentStatus = STATUS.SAVING;
+    this.bookFile.reader.onloadend = () => this.currentStep++;
   }
   private onBookCoverUpload(files: FileList) {
     if (!files.length) {
@@ -110,8 +116,14 @@ export default class BookUpload extends Vue {
     const file = files[0];
     this.bookCoverFile.name = file.name;
     this.bookCoverFile.reader.readAsDataURL(file);
-    this.currentStatus = STATUS.SAVING;
     return;
+  }
+
+  private created() {
+    this.bookCoverFile.reader.onloadend = (event: ProgressEvent) => {
+      const result = (event.target as FileReader).result as string;
+      this.coverImgData = result;
+    };
   }
 }
 </script>
@@ -150,9 +162,11 @@ button {
 .input-file {
   opacity: 0; /* invisible but it's there! */
   width: 100%;
-  height: 200px;
+  height: 100%;
   position: absolute;
   cursor: pointer;
+  top: 0px;
+  left: 0px;
 }
 
 .dropbox:hover {
@@ -163,5 +177,31 @@ button {
   font-size: 1.2em;
   text-align: center;
   padding: 50px 0;
+}
+
+.dropbox-img {
+  color: dimgray;
+  width: 100%;
+  padding: 10px 10px;
+  height: 100%;
+  position: relative;
+  cursor: pointer;
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+}
+
+.dropbox-img > p {
+  padding: 5px;
+}
+
+.dropbox-no-image {
+  outline: 2px dashed grey;
+  outline-offset: -10px;
+  background-color: lightcyan;
+}
+
+.dropbox-no-image:hover {
+  background-color: lightblue;
 }
 </style>
